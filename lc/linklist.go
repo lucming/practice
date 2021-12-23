98c421249b5e7a8e5d6fec53a86328deba09f1c0
liuming6 <liuming6@360.cn>
2021-12-23 20:08:33 +0800

    link list

--- lc/linklist.go
+++ lc/linklist.go
@@ -108,3 +108,114 @@ func (n *node) show() {
 		fmt.Println(p.data)
 	}
 }
+
+func cycle(head *node) bool {
+	if head == nil {
+		return false
+	}
+
+	slow, quick := head, head
+	for quick != nil && quick.next != nil {
+		slow = slow.next
+		quick = quick.next.next
+		if slow == quick {
+			return true
+		}
+	}
+
+	return false
+}
+
+//给定单链表(head)，如果有环的话请返回从头结点进入环的第一个节点。
+//先判断是否有环。
+//如果有环，那么p1p2重合点p必然在环中。从p点断开环，方法为：p1=p, p2=p->next, p->next=NULL。此时，原单链表可以看作两条单链表，一条从head开始，另一条从p2开始，我们找到它们的第一个交点即为所求。
+func getLoppNode(head *node) *node {
+	if head == nil {
+		return nil
+	}
+
+	slow, quick := head, head
+	for quick != nil && quick.next != nil {
+		slow = slow.next
+		quick = quick.next.next
+		if slow == quick {
+			slow = head
+			for {
+				quick = quick.next
+				slow = slow.next
+				if quick == slow {
+					break
+				}
+			}
+			return slow
+		}
+	}
+
+	return nil
+}
+
+//给定两个单链表(head1, head2)，检测两个链表是否有交点，如果有返回第一个交点。
+//方法一：
+//如果head1==head2，那么显然相交，直接返回head1。
+//否则，分别从head1,head2开始遍历两个链表获得其长度len1与len2。假设len1>=len2，那么指针p1由head1开始向后 移动len1-len2步。指针p2=head2，下面p1、p2每次向后前进一步并比较p1p2是否相等，如果相等即返回该结点，否则说明两个链表没有 交点。
+//方法二：
+//将其中一条单链表首尾相连，转化成求链表入环的第一个结点
+func getPoint(head1, head2 *node) *node {
+	p := head1
+	for p.next != nil {
+		p = p.next
+	}
+	p.next = head1
+
+	return getLoppNode(head2)
+}
+
+//只给定单链表中某个结点p(并非最后一个结点，即p->next!=NULL)指针，删除该结点。
+//首先是放p中数据,然后将p->next的数据copy入p中，接下来删除p->next即可。
+func deletePoint(head *node, find *node) {
+	if head == nil {
+		return
+	}
+
+	p := head
+	for p.next != nil {
+		if p.next == find {
+			p.next = find.next
+		}
+		p = p.next
+	}
+}
+
+//只给定单链表中某个结点p(非空结点)，在p前面插入一个结点。
+//办法与前者类似，首先分配一个结点q，将q插入在p后，接下来将p中的数据copy入q中，然后再将要插入的数据记录在p中
+//只给定单链表中某个结点p（非空）,在p前插一个结点
+func insertPri(head, p, s *node) {
+	ptmp := head
+	for ptmp.next != nil {
+		if ptmp.next == p {
+			s.next = ptmp.next
+			ptmp.next = s
+			break
+		}
+		ptmp = ptmp.next
+	}
+}
+
+//给定单链表头结点，删除链表中倒数第k个节点
+//思路:快慢指针，快指针先走k步，然后快慢指针一起走，快指针走的末尾，慢指针就在第n-1步，然后慢指针跳一个节点
+func deleteK(head *node, k int) {
+	if getLength(head) < k {
+		return
+	}
+
+	slow, fast := head, head
+	for i := 0; i < k; i++ {
+		fast = fast.next
+	}
+
+	for fast.next != nil {
+		slow = slow.next
+		fast = fast.next
+	}
+	slow.next = slow.next.next
+}
--- lc/linklist_test.go
+++ lc/linklist_test.go
@@ -1,6 +1,7 @@
 package lc
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -12,4 +13,181 @@ func Test_node_headInsert(t *testing.T) {
 	head.show()
 	reverse2(&head)
 	head.show()
+}
+
+func getCycleList() *node {
+	n8 := node{data: 8}
+	n7 := node{data: 7, next: &n8}
+	n6 := node{data: 6, next: &n7}
+	n5 := node{data: 5, next: &n6}
+	n4 := node{data: 4, next: &n5}
+	n3 := node{data: 3, next: &n4}
+	n2 := node{data: 2, next: &n3}
+	n1 := node{data: 1, next: &n2}
+	head := node{next: &n1}
+	n8.next = &n5
+	return &head
+}
+
+func Test_cycle(t *testing.T) {
+	p1 := node{}
+	for i := 0; i < 10; i++ {
+		p1.headInsert(i)
+	}
+
+	p2 := node{}
+	p2.next = &p2
+
+	type args struct {
+		head *node
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "not cycle",
+			args: args{head: &p1},
+			want: false,
+		},
+		{
+			name: "cycle",
+			args: args{head: &p2},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := cycle(tt.args.head); got != tt.want {
+				t.Errorf("cycle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getLoppNode(t *testing.T) {
+
+	type args struct {
+		head *node
+	}
+	tests := []struct {
+		name string
+		args args
+		want *node
+	}{
+		// TODO: Add test cases.
+		{
+			name: "first node",
+			args: args{head: getCycleList()},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getLoppNode(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getLoppNode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_deletePoint(t *testing.T) {
+	n5 := node{data: 5}
+	n4 := node{data: 4, next: &n5}
+	n3 := node{data: 3, next: &n4}
+	n2 := node{data: 2, next: &n3}
+	n1 := node{data: 1, next: &n2}
+	head := node{next: &n1}
+
+	type args struct {
+		head *node
+		find *node
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{
+			name: "delete exist node",
+			args: args{
+				head: &head,
+				find: &n1,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			deletePoint(tt.args.head, tt.args.find)
+			tt.args.head.show()
+		})
+	}
+}
+
+func Test_insertPri(t *testing.T) {
+	n5 := node{data: 5}
+	n4 := node{data: 4, next: &n5}
+	n3 := node{data: 3, next: &n4}
+	n2 := node{data: 2, next: &n3}
+	n1 := node{data: 1, next: &n2}
+	head := node{next: &n1}
+
+	type args struct {
+		head *node
+		p    *node
+		s    *node
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{
+			name: "add",
+			args: args{
+				head: &head,
+				p:    &n1,
+				s:    &node{data: 100},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			insertPri(tt.args.head, tt.args.p, tt.args.s)
+			tt.args.head.show()
+		})
+	}
+}
+
+func Test_deleteK(t *testing.T) {
+	p1 := node{}
+	for i := 0; i < 10; i++ {
+		p1.headInsert(i)
+	}
+
+	type args struct {
+		head *node
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{
+			name: "normal",
+			args: args{
+				head: &p1,
+				k:    3,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			deleteK(tt.args.head, tt.args.k)
+			tt.args.head.show()
+		})
+	}
 }
