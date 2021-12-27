a3a7d7a259f0ff381d7218cd424b39c59e660ec7
liuming6 <liuming6@360.cn>
2021-12-27 17:16:36 +0800

    binary travel

--- main.go
+++ main.go
@@ -7,15 +7,23 @@ import (
 
 func main() {
 	fmt.Println("hello world")
-	p1:=tree.NewNode(1)
-	p2:=tree.NewNode(2)
-	p3:=tree.NewNode(3)
-	p4:=tree.NewNode(4)
-	p5:=tree.NewNode(5)
-	p1.Left=p2
-	p1.Right=p3
-	p2.Left=p4
-	p2.Right=p5
+	p1 := tree.NewNode(1)
+	p2 := tree.NewNode(2)
+	p3 := tree.NewNode(3)
+	p4 := tree.NewNode(4)
+	p5 := tree.NewNode(5)
+	p1.Left = p2
+	p1.Right = p3
+	p2.Left = p4
+	p2.Right = p5
 
-	p1.PostOrder()
+	//p1.PostOrder()
+	result := tree.Bfs(p1)
+	fmt.Println("层次遍历：", result)
+	result = tree.PreOrder(p1)
+	fmt.Println("先序遍历：", result)
+	result = tree.Inorder(p1)
+	fmt.Println("中序遍历：", result)
+	result = tree.PostOrder(p1)
+	fmt.Println("后序遍历：", result)
 }
