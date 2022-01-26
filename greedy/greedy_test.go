69fc906ca98527d88fc4d4d14f015b8c08f65866
lucming <2876757716@qq.com>
2022-01-26 18:50:16 +0800

    跳跃游戏

--- greedy/greedy_test.go
+++ greedy/greedy_test.go
@@ -128,3 +128,33 @@ func Test_maxProfit(t *testing.T) {
 		})
 	}
 }
+
+func Test_canJump(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{[]int{2, 3, 1, 1, 4}},
+			want: true,
+		},
+		{
+			name: "test2",
+			args: args{[]int{3, 2, 1, 0, 4}},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canJump(tt.args.nums); got != tt.want {
+				t.Errorf("canJump() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
