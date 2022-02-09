c7f626db7027eb549edd4dd64ace6723e95544da
lucming <2876757716@qq.com>
2022-02-09 18:03:40 +0800

    合并区间

--- greedy/greedy.go
+++ greedy/greedy.go
@@ -399,3 +399,34 @@ func eraseOverlapIntervals(intervals [][]int) int {
 
 	return result
 }
+
+//给出一个区间的集合，请合并所有重叠的区间。
+//示例 1:
+//输入: intervals = [[1,3],[2,6],[8,10],[15,18]]
+//输出: [[1,6],[8,10],[15,18]]
+//解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+//示例 2:
+//输入: intervals = [[1,4],[4,5]]
+//输出: [[1,5]]
+//解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+//提示：
+//intervals[i][0] <= intervals[i][1]
+func merge(nums [][]int) [][]int {
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i][0] < nums[j][0]
+	})
+
+	result := make([][]int, 0)
+	current := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i-1][1] >= nums[i][0] {
+			current[1] = max(nums[i-1][1], nums[i][1])
+		} else {
+			result = append(result, current)
+			current = nums[i]
+		}
+	}
+	result = append(result, current)
+
+	return result
+}
--- greedy/greedy_test.go
+++ greedy/greedy_test.go
@@ -1,6 +1,9 @@
 package greedy
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_findContentChildren(t *testing.T) {
 	type args struct {
@@ -304,6 +307,36 @@ func Test_eraseOverlapIntervals(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := eraseOverlapIntervals(tt.args.intervals); got != tt.want {
 				t.Errorf("eraseOverlapIntervals() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_merge(t *testing.T) {
+	type args struct {
+		nums [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{[][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}}},
+			want: [][]int{{1, 6}, {8, 10}, {15, 18}},
+		},
+		{
+			name: "test2",
+			args: args{[][]int{{1, 4}, {4, 5}}},
+			want: [][]int{{1, 5}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := merge(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("merge() = %v, want %v", got, tt.want)
 			}
 		})
 	}
