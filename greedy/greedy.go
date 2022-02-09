847397556a6b840196c0cb7a53a102f78c887461
lucming <2876757716@qq.com>
2022-02-09 19:40:15 +0800

    单调递增的数字

--- greedy/greedy.go
+++ greedy/greedy.go
@@ -2,6 +2,7 @@ package greedy
 
 import (
 	"sort"
+	"strconv"
 )
 
 //分饼干问题
@@ -428,5 +429,37 @@ func merge(nums [][]int) [][]int {
 	}
 	result = append(result, current)
 
+	return result
+}
+
+//给定一个非负整数 N，找出小于或等于 N 的最大的整数，同时这个整数需要满足其各个位数上的数字是单调递增。
+//（当且仅当每个相邻位数上的数字 x 和 y 满足 x <= y 时，我们称这个整数是单调递增的。）
+//示例 1:
+//输入: N = 10
+//输出: 9
+//示例 2:
+//输入: N = 1234
+//输出: 1234
+//示例 3:
+//输入: N = 332
+//输出: 299
+//说明: N 是在 [0, 10^9] 范围内的一个整数。
+func monotoneIncreasingDigits(num int) int {
+	stringNum := strconv.Itoa(num)
+	byteNum := []byte(stringNum)
+	if len(byteNum) <= 1 {
+		return num
+	}
+
+	for i := len(byteNum) - 1; i > 0; i-- {
+		if byteNum[i] < byteNum[i-1] {
+			byteNum[i-1]--
+			for j := i; j < len(byteNum); j++ {
+				byteNum[j] = '9'
+			}
+		}
+	}
+
+	result, _ := strconv.Atoi(string(byteNum))
 	return result
 }
--- greedy/greedy_test.go
+++ greedy/greedy_test.go
@@ -341,3 +341,38 @@ func Test_merge(t *testing.T) {
 		})
 	}
 }
+
+func Test_monotoneIncreasingDigits(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{10},
+			want: 9,
+		},
+		{
+			name: "test2",
+			args: args{1234},
+			want: 1234,
+		},
+		{
+			name: "test3",
+			args: args{332},
+			want: 299,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := monotoneIncreasingDigits(tt.args.num); got != tt.want {
+				t.Errorf("monotoneIncreasingDigits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
