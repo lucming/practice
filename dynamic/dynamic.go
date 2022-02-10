32ac8a4ce23f22afd90df98175451ccce0713381
lucming <2876757716@qq.com>
2022-02-10 17:19:54 +0800

    爬楼梯

--- dynamic/dynamic.go
+++ dynamic/dynamic.go
@@ -1,5 +1,7 @@
 package dynamic
 
+import "github.com/lucming/practice/tools"
+
 //斐波那契数列
 //f(n)=f(n-1)+f(n-2)
 func fib(n int) int {
@@ -15,4 +17,26 @@ func fib(n int) int {
 	}
 
 	return current
+}
+
+//花费最小体力爬楼梯
+func minCostClimbingStairs(cost []int) int {
+	morepre, pre, current := cost[0], cost[1], 0
+	for i := 2; i < len(cost); i++ {
+		current = tools.Min(morepre, pre) + cost[i]
+		morepre = pre
+		pre = current
+	}
+
+	return tools.Min(morepre, pre)
+}
+
+func minCostClimbingStairs1(cost []int) int {
+	dp := make([]int, len(cost))
+	dp[0], dp[1] = cost[0], cost[1]
+	for i := 2; i < len(cost); i++ {
+		dp[i] = tools.Min(dp[i-2], dp[i-1]) + cost[i]
+	}
+
+	return tools.Min(dp[len(dp)-1], dp[len(dp)-2])
 }
--- dynamic/dynamic_test.go
+++ dynamic/dynamic_test.go
@@ -26,3 +26,33 @@ func Test_fib(t *testing.T) {
 		})
 	}
 }
+
+func Test_minCostClimbingStairs(t *testing.T) {
+	type args struct {
+		cost []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{[]int{10, 15, 20}},
+			want: 15,
+		},
+		{
+			name: "test2",
+			args: args{[]int{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}},
+			want: 6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minCostClimbingStairs1(tt.args.cost); got != tt.want {
+				t.Errorf("minCostClimbingStairs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
