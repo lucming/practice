46e36b867743e4cc7a1af491a87888c3c6d157a0
lucming <2876757716@qq.com>
2022-02-21 17:53:24 +0800

    目标和

--- dynamic/dynamic.go
+++ dynamic/dynamic.go
@@ -182,3 +182,37 @@ func lastStoneWeight(stones []int) int {
 
 	return sumStone - 2*dp[target]
 }
+
+//给定一个非负整数数组，a1, a2, ..., an, 和一个目标数，S。现在你有两个符号 + 和 -。对于数组中的任意一个整数，你都可以从 + 或 -中选择一个符号添加在前面。
+//返回可以使最终数组和为目标数 S 的所有添加符号的方法数。
+//示例：
+//输入：nums: [1, 1, 1, 1, 1], S: 3
+//输出：5
+//解释：
+//-1+1+1+1+1 = 3
+//+1-1+1+1+1 = 3
+//+1+1-1+1+1 = 3
+//+1+1+1-1+1 = 3
+//+1+1+1+1-1 = 3
+//一共有5种方法让最终目标和为3。
+func findTargetSumWays(nums []int, target int) int {
+	sum := tools.Sum(nums)
+	if target > sum {
+		return 0
+	}
+
+	if (sum+target)%2 == 1 {
+		return 0
+	}
+
+	bag := (sum + target) / 2
+	dp := make([]int, bag+1)
+	dp[0] = 1
+	for i := 0; i < len(nums); i++ {
+		for j := bag; j >= nums[i]; j-- {
+			dp[j] += dp[j-nums[i]]
+		}
+	}
+
+	return dp[bag]
+}
