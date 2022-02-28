fbe54c6be30c872ecf919d37b56c21c084d566e5
lucming <2876757716@qq.com>
2022-02-28 22:27:00 +0800

    打家劫舍

--- dynamic/dynamic_test.go
+++ dynamic/dynamic_test.go
@@ -414,3 +414,33 @@ func Test_claimstairs(t *testing.T) {
 		})
 	}
 }
+
+func Test_rob(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{[]int{1, 2, 3, 1}},
+			want: 4,
+		},
+		{
+			name: "test2",
+			args: args{[]int{2, 7, 9, 3, 1}},
+			want: 12,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rob(tt.args.nums); got != tt.want {
+				t.Errorf("rob() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
