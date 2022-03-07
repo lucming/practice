da4238711073a55fe26f31775a2a358bf40af749
liuming6 <liuming6@360.cn>
2022-03-07 22:52:28 +0800

    最长公共子序列

--- dynamic/dynamic_test.go
+++ dynamic/dynamic_test.go
@@ -588,3 +588,48 @@ func Test_findLength(t *testing.T) {
 		})
 	}
 }
+
+func Test_longestCommonSubsequence(t *testing.T) {
+	type args struct {
+		text1 string
+		text2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{
+				text1: "abcde",
+				text2: "ace",
+			},
+			want: 3,
+		},
+		{
+			name: "test2",
+			args: args{
+				text1: "ace",
+				text2: "ace",
+			},
+			want: 3,
+		},
+		{
+			name: "test3",
+			args: args{
+				text1: "abc",
+				text2: "def",
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestCommonSubsequence(tt.args.text1, tt.args.text2); got != tt.want {
+				t.Errorf("longestCommonSubsequence() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
