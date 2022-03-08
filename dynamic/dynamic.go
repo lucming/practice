9154c2e68a053010c87a1faf02d10a19fd757c16
liuming6 <liuming6@360.cn>
2022-03-08 11:39:16 +0800

    判断是否为子序列

--- dynamic/dynamic.go
+++ dynamic/dynamic.go
@@ -587,3 +587,26 @@ func maxSubArray(nums []int) int {
 
 	return maxSum
 }
+
+//给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+//字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+//示例 1： 输入：s = "abc", t = "ahbgdc" 输出：true
+//示例 2： 输入：s = "axc", t = "ahbgdc" 输出：false
+func isSubsequence(s, t string) bool {
+	dp := make([][]int, len(s)+1)
+	for i := 0; i < len(s)+1; i++ {
+		dp[i] = make([]int, len(t)+1)
+	}
+
+	for i := 1; i <= len(s); i++ {
+		for j := 1; j <= len(t); j++ {
+			if s[i-1] == t[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = dp[i][j-1]
+			}
+		}
+	}
+
+	return dp[len(s)][len(t)] == len(s)
+}
