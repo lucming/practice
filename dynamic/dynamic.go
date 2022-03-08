c209f08fed498e3f28b2ce60b8c1beb49f0bb7ec
liuming6 <liuming6@360.cn>
2022-03-08 10:59:50 +0800

    最大连续子数组和

--- dynamic/dynamic.go
+++ dynamic/dynamic.go
@@ -565,3 +565,25 @@ func longestCommonSubsequence(text1, text2 string) int {
 
 	return dp[len(text1)][len(text2)]
 }
+
+//最大连续子数组和
+//给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+//示例: 输入: [-2,1,-3,4,-1,2,1,-5,4] 输出: 6 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+//dp[i]=max(dp[i-1]+nums[i],nums[i])
+func maxSubArray(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	maxSum := 0
+	for i := 1; i < len(nums); i++ {
+		dp[i] = tools.Max(dp[i-1]+nums[i], nums[i])
+		if dp[i] > maxSum {
+			maxSum = dp[i]
+		}
+	}
+
+	return maxSum
+}
