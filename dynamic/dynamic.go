fa7ceb29ebc46ac01b3c7b03ac1d9311a15c4f5f
liuming6 <liuming6@360.cn>
2022-03-09 22:07:20 +0800

    不同子序列

--- dynamic/dynamic.go
+++ dynamic/dynamic.go
@@ -610,3 +610,27 @@ func isSubsequence(s, t string) bool {
 
 	return dp[len(s)][len(t)] == len(s)
 }
+
+//不同的子序列
+//给定一个字符串 s 和一个字符串 t ，计算在 s 的子序列中 t 出现的个数。
+func numDistinct(s, t string) int {
+	dp := make([][]int, len(s)+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(t)+1)
+	}
+	for i := 0; i < len(dp); i++ {
+		dp[i][0] = 1
+	}
+
+	for i := 1; i <= len(s); i++ {
+		for j := 1; j <= len(t); j++ {
+			if s[i-1] == t[j-1] {
+				dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+			} else {
+				dp[i][j] = dp[i-1][j]
+			}
+		}
+	}
+
+	return dp[len(s)][len(t)]
+}
--- dynamic/dynamic_test.go
+++ dynamic/dynamic_test.go
@@ -695,3 +695,40 @@ func Test_isSubsequence(t *testing.T) {
 		})
 	}
 }
+
+func Test_numDistinct(t *testing.T) {
+	type args struct {
+		s string
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{
+				s: "rabbbit",
+				t: "rabbit",
+			},
+			want: 3,
+		},
+		{
+			name: "test2",
+			args: args{
+				s: "babgbag",
+				t: "bag",
+			},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numDistinct(tt.args.s, tt.args.t); got != tt.want {
+				t.Errorf("numDistinct() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
