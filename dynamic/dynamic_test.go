8cb8759cce79745a075abbc2288a8c3a1d4cca5d
liuming6 <liuming6@360.cn>
2022-03-11 21:44:18 +0800

    最长回文子串

--- dynamic/dynamic_test.go
+++ dynamic/dynamic_test.go
@@ -761,3 +761,33 @@ func Test_minDistance(t *testing.T) {
 		})
 	}
 }
+
+func Test_longestPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{"babad"},
+			want: "bab",
+		},
+		{
+			name: "test2",
+			args: args{"cbbd"},
+			want: "bb",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("longestPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
