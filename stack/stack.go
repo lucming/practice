519b43ea0ee3037e0b35c9bd91708e552af90ffc
liuming6 <liuming6@360.cn>
2022-03-16 22:07:15 +0800

    下一个更大的元素

--- stack/stack.go
+++ stack/stack.go
@@ -103,3 +103,47 @@ func dailyTemperatures(nums []int) []int {
 
 	return result
 }
+
+//给你两个 没有重复元素 的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。
+//请你找出 nums1 中每个元素在 nums2 中的下一个比其大的值。
+//nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。如果不存在，对应位置输出 -1 。
+//示例 1:
+//输入: nums1 = [4,1,2], nums2 = [1,3,4,2].
+//输出: [-1,3,-1]
+//解释:
+//对于 num1 中的数字 4 ，你无法在第二个数组中找到下一个更大的数字，因此输出 -1 。
+//对于 num1 中的数字 1 ，第二个数组中数字1右边的下一个较大数字是 3 。
+//对于 num1 中的数字 2 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
+//示例 2:
+//输入: nums1 = [2,4], nums2 = [1,2,3,4].
+//输出: [3,-1]
+//解释:
+//对于 num1 中的数字 2 ，第二个数组中的下一个较大数字是 3 。
+//对于 num1 中的数字 4 ，第二个数组中没有下一个更大的数字，因此输出-1
+//思路：先把num1的value和idx做映射，然后遍历num2，
+//通过前面的单调栈规律，如果nums2的value中也出现在nums1中
+//如果nums2中的当前值>栈顶元素，说明找到了，就把当前值写在nums2在nums1中的下标,
+func nextGreaterElement(nums1, nums2 []int) []int {
+	m := make(map[int]int)
+	for k, v := range nums1 {
+		m[v] = k
+	}
+
+	result := make([]int, len(nums1))
+	for i := 0; i < len(nums1); i++ {
+		result[i] = -1
+	}
+	stack := []int{0}
+	for i := 1; i < len(nums2); i++ {
+		for len(stack) > 0 && nums2[i] > nums2[stack[len(stack)-1]] {
+			top := stack[len(stack)-1]
+			if _, ok := m[nums2[top]]; ok {
+				result[m[nums2[top]]] = nums2[i]
+			}
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, i)
+	}
+
+	return result
+}
