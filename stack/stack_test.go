3f3ed18c715abfced1a93a386df859eb6c64665a
liuming6 <liuming6@360.cn>
2022-03-20 18:00:28 +0800

    下一个更大元素

--- stack/stack_test.go
+++ stack/stack_test.go
@@ -210,3 +210,28 @@ func Test_nextGreaterElement(t *testing.T) {
 		})
 	}
 }
+
+func Test_nextGreaterElements(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{[]int{1, 2, 1}},
+			want: []int{2, -1, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := nextGreaterElements(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("nextGreaterElements() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
