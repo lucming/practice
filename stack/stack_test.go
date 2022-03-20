fca79de5674696f71c2e3951be899c7842405e61
liuming6 <liuming6@360.cn>
2022-03-20 19:34:25 +0800

    接雨水

--- stack/stack_test.go
+++ stack/stack_test.go
@@ -235,3 +235,33 @@ func Test_nextGreaterElements(t *testing.T) {
 		})
 	}
 }
+
+func Test_trap(t *testing.T) {
+	type args struct {
+		height []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{[]int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}},
+			want: 6,
+		},
+		{
+			name: "test2",
+			args: args{[]int{4, 2, 0, 3, 2, 5}},
+			want: 9,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := trap3(tt.args.height); got != tt.want {
+				t.Errorf("trap() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
