929529227dee40b69409de9f07b3bf8e70af161e
liuming6 <liuming6@360.cn>
2022-03-27 23:17:26 +0800

    计算素数

---
+++ leetcode/lc204_test.go
@@ -0,0 +1,38 @@
+package leetcode
+
+import "testing"
+
+func Test_countPrimes(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{10},
+			want: 4,
+		},
+		{
+			name: "test2",
+			args: args{0},
+			want: 0,
+		},
+		{
+			name: "test3",
+			args: args{1},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countPrimes(tt.args.n); got != tt.want {
+				t.Errorf("countPrimes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
