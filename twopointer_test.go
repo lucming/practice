9b2df4386348a82e6d3673dc88524333e03370a2
liuming6 <liuming6@360.cn>
2022-03-30 22:16:44 +0800

    有序数组去重

--- twopointer_test.go
+++ twopointer_test.go
@@ -31,3 +31,33 @@ func Test_removeDuplicates(t *testing.T) {
 		})
 	}
 }
+
+func Test_removeDuplicates1(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{[]int{1, 1, 2, 2, 2, 3}},
+			want: 5,
+		},
+		{
+			name: "test2",
+			args: args{[]int{1, 1, 1, 1, 1, 1}},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeDuplicates2(tt.args.nums); got != tt.want {
+				t.Errorf("removeDuplicates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
