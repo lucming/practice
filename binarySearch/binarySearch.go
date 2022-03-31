5ec43ec852acf19a9d89935300a0471521d55713
liuming6 <liuming6@360.cn>
2022-03-31 20:09:11 +0800

    有序数组查找元素开始结束位置

--- binarySearch/binarySearch.go
+++ binarySearch/binarySearch.go
@@ -122,3 +122,55 @@ func isSubsequence(s, t string) bool {
 
 	return i == lenS
 }
+
+//在排序数组中查找元素的第一个和最后一个位置
+func searchRange(nums []int, target int) []int {
+	left := leftSearch(nums, target)
+	right := rightSearch(nums, target)
+
+	return []int{left, right}
+}
+
+func leftSearch(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] >= target {
+			right = mid - 1
+		}
+	}
+
+	if left >= len(nums) || nums[left] != target {
+		return -1
+	}
+
+	return left
+}
+
+func rightSearch(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] <= target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		}
+	}
+
+	if right < 0 || nums[right] != target {
+		return -1
+	}
+
+	return right
+}
