59bd5bc7dcf9e56c85ce1170435a83ea67d476ff
liuming6 <liuming6@360.cn>
2022-04-14 14:06:22 +0800

    change variable name

--- leetcode/lc3.go
+++ leetcode/lc3.go
@@ -6,9 +6,9 @@ func lengthOfLongestSubstring(s string) int {
 	m := make(map[byte]int)
 	result := 0
 	right := 0
-	for idx := 0; idx < len(s); idx++ {
-		if idx != 0 {
-			delete(m, s[idx-1])
+	for left := 0; left < len(s); left++ {
+		if left != 0 {
+			delete(m, s[left-1])
 		}
 		for j := right; j < len(s); j++ {
 			if m[s[j]] != 0 {
@@ -18,7 +18,7 @@ func lengthOfLongestSubstring(s string) int {
 			right++
 		}
 
-		result = tools.Max(result, right-idx)
+		result = tools.Max(result, right-left)
 	}
 
 	return result
