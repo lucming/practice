4fe1aa6b4a9bb16506e1b10836e7310ef4ce0b75
liuming6 <liuming6@360.cn>
2022-04-14 18:18:40 +0800

    fix bug of leetcode33

--- leetcode/lc33.go
+++ leetcode/lc33.go
@@ -2,17 +2,29 @@ package leetcode
 
 //旋转数组查找
 func search(nums []int, target int) int {
+	lenNums := len(nums)
+	if lenNums == 0 {
+		return -1
+	}
+	if lenNums == 1 {
+		if nums[0] == target {
+			return 0
+		} else {
+			return -1
+		}
+	}
+
 	left, right := 0, len(nums)-1
 	for left <= right {
 		mid := (left + right) / 2
-		if nums[mid] == target {
-			return mid
-		}
 		if nums[left] == nums[mid] && nums[mid] == nums[right] {
 			left++
 			right--
 		}
-		if nums[left] <= nums[mid] {
+
+		if nums[mid] == target {
+			return mid
+		} else if nums[left] <= nums[mid] {
 			if nums[left] <= target && target < nums[mid] {
 				right = mid - 1
 			} else {
