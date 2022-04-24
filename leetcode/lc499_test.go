ba30aad4f7372a55fde1da0381592ecae407cc17
liuming6 <liuming6@360.cn>
2022-04-24 20:30:23 +0800

    leetcode 499:find target sum ways

---
+++ leetcode/lc499_test.go
@@ -0,0 +1,40 @@
+package leetcode
+
+import "testing"
+
+func Test_findTargetSumWays(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{
+				nums:   []int{1, 1, 1, 1, 1},
+				target: 3,
+			},
+			want: 5,
+		},
+		{
+			name: "test2",
+			args: args{
+				nums:   []int{1},
+				target: 1,
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findTargetSumWays(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("findTargetSumWays() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
